13bfa0baeced0bd1e60a9f716c8eb8e33e5b383e
Rasmussen <Nicholas.Rasmussen@water.ca.gov>
2021-03-29 11:46:26 -0700

    Subsetted the sample data to just those stations relevant to the SMSCG action

--- SMSCG_Phytoplankton_DataVisualization.R
+++ SMSCG_Phytoplankton_DataVisualization.R
@@ -11,32 +11,65 @@
 
 #load packages
 library(tidyverse)
+library(ggplot2)
 
-#set working directory
-setwd("C:/Users/nrasmuss/OneDrive - California Department of Water Resources/SMSCG/Phyto/Data_2020")
+# 1. Read in the Data----------------------------------------------
+# Dataset is on SharePoint site for the SMSCG action
+
+# Define path on SharePoint site for data
+sharepoint_path <- normalizePath(
+  file.path(
+    Sys.getenv("USERPROFILE"),
+    "California Department of Water Resources/SMSCG - Summer Action - Data/Phytoplankton"
+  )
+)  
 
 phyto_vis<-read_csv("SMSCG_phytoplankton_formatted_2020.csv")
 #looks like column types are all correct, even date and time
 
 #look at station names
-unique(phyto_vis$station2)
-#two versions of STN 610: STN 610, STN610
+sort(unique(phyto_vis$station))
+#names are mostly, but not completely, unique
+#fix this one because it's needed: "STN 610" vs "STN610"
+#the rest will be filtered out: 
+#"C3A" "C3A-Hood" "C3A-HOOD"
+#"D16"  "D16-Twitchell"
+#"E26" vs "EZ6" 
 
 #remove spaces from all station names, which will remove the redundant versions of 610
 phyto_vis$station2<-str_replace_all(phyto_vis$station, " ", "")
 
 #EMP stations: Subset to just the four relevant to SMSCG
-#Two different naming systems for stations: EMP = DFW
+#Two different naming systems for stations: EMP (DFW)
 #D22 (NZ064), D4 (NZ060), NZ032 (NZ032), NZS42 (NZS42)
 
 #create data frame that groups names of stations that represent the same general location and adds the three broad regions
+#RV = Sacramento River, ME = East Suisun Marsh, MW = West Suisun Marsh
 station_key <- data.frame(
-  station = c("STN706", "FMWT706", "NZ064", "D22","STN704","FMWT704","STN801", "FMWT802", "NZ060", "D4","STN609","MONT","STN610","FMWT605", "STN606","FMWT606","NZ032","NZS42"),
+  station2 = c("STN706", "FMWT706", "NZ064", "D22","STN704","FMWT704","STN801", "FMWT802", "NZ060", "D4","STN609","MONT","STN610","FMWT605", "STN606","FMWT606","NZ032","NZS42"),
   station_comb = c(rep("PHY706", 4), rep("PHY704",2), rep("PHY801", 4),"STN609","MONT","STN610", "FMWT605", rep("PHY606",3),"NZS42"),
-  region = c(rep("river",10), rep("east_marsh",3), rep("west_marsh",5))
+  region = c(rep("RV",10), rep("ME",3), rep("MW",5))
 )
+glimpse(station_key)
 
-#
+#join the sample data and the new station/region data
+#by using inner_join, only the SMSCG relevant stations are kept
+phyto_gates<- inner_join(phyto_vis,station_key) 
+
+#look at number of samples per station
+s_samp_count<-phyto_gates %>% 
+  distinct(station_comb, date) %>% 
+  group_by(station_comb) %>% 
+  summarize(count = n())  
+#generally 7-8 as expected 
+
+#look at number of samples per region
+r_samp_count<-phyto_gates %>% 
+  distinct(region, date) %>% 
+  group_by(region) %>% 
+  summarize(count = n())  
+#twice as many samples in river as east marsh
+#west marsh intermediate in sample number
 
 
 
