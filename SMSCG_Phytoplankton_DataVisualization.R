62afbe11d026ebefdde1060736957c290937f2fe
Rasmussen <Nicholas.Rasmussen@water.ca.gov>
2021-03-29 15:11:21 -0700

    Plotted number of genera by station and region

--- SMSCG_Phytoplankton_DataVisualization.R
+++ SMSCG_Phytoplankton_DataVisualization.R
@@ -132,3 +132,79 @@ r_phyto_sum<-s_phyto_sum %>%
     facet_wrap(~region)
 )
 
+#boxplots of total density and biovolume by region-------
+
+(plot_rg_tot_den_bx<-ggplot(data=s_phyto_sum, aes(x = region, y = tot_den)) + 
+   geom_boxplot()+
+   geom_jitter() #adds all points to plot, not just outliers
+)
+
+(plot_rg_tot_bvol_bx<-ggplot(data=s_phyto_sum, aes(x = region, y = tot_bvol)) + 
+    geom_boxplot()+
+    geom_jitter() #adds all points to plot, not just outliers
+)
+
+
+#plot number of genera by station and region-------
+
+#determine how many species typically within each genera
+#if generally one, then easier to just use genus for plots/analysis
+ssp_count<-phyto_gates %>% 
+  distinct(genus, taxon) %>% 
+  group_by(genus) %>% 
+  summarize(count = n())  
+
+#use histogram to visualize distribution of species per genera
+(plot_spp_per_genus_hist<-ggplot(data=ssp_count, aes(x = count)) + 
+    geom_histogram()
+)
+#most genera contain 1-2 species but some contain up to 15
+
+#look closer at genera with many species
+genera_rich<-phyto_gates %>% 
+  distinct(genus, taxon) %>% 
+  filter(genus=="Nitzschia" | genus =="Navicula") %>% 
+  arrange(genus)
+#not just unique because of variants of same name
+#actually quite a few different species within these genera present
+
+#create df that counts genera by sample
+phyto_genera_sum<-phyto_gates %>% 
+  distinct(region, station_comb,date, month,genus) %>% 
+  group_by(region, station_comb,date, month) %>% 
+  summarize(genera = n())
+
+#use histogram to visualize distribution of genera per sample
+(plot_genera_per_sample_hist<-ggplot(data=phyto_genera_sum, aes(x = genera)) + 
+    geom_histogram()
+)
+#generally 1-7 but some up to 15 genera per sample
+
+#plot time series of number of genera by station
+(plot_st_genera <-ggplot(phyto_genera_sum, aes(x=date, y=genera))+ 
+    geom_line() + 
+    geom_point() + 
+    facet_wrap(~station_comb)
+)
+
+#number of genera per region
+phyto_genera_sum_rg<-phyto_gates %>% 
+  distinct(region,date, month,genus) %>% 
+  group_by(region,date, month) %>% 
+  summarize(genera = n())
+
+#plot time series of number of genera by region
+(plot_st_genera <-ggplot(phyto_genera_sum_rg, aes(x=date, y=genera))+ 
+    geom_line() + 
+    geom_point() + 
+    facet_wrap(~region)
+)
+
+#stacked bar plot showing phyla by station-------
+
+
+#stacked bar plot showing phyla by region-------
+
+
+
+
