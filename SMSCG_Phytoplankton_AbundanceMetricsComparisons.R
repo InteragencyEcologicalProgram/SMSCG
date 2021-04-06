f0b21f9d36443e49ea3f48266131f76d4a372e2b
Rasmussen <Nicholas.Rasmussen@water.ca.gov>
2021-04-05 18:36:20 -0700

    created R script to make comparisons among different phytoplankton abundance metrics

---
+++ SMSCG_Phytoplankton_AbundanceMetricsComparisons.R
@@ -0,0 +1,33 @@
+#SMSCG
+#phytoplantkon
+#comparisons among data types:
+#Organisms/mL
+#Biovolume (microns^3)/mL
+#Carbon/mL
+#Extracted Chlor-a
+#Sonde Fluorescence
+
+#load packages
+library(tidyverse)
+library(readxl)
+library(ggplot2)
+library(lubridate)
+
+# 1. Read in the Data----------------------------------------------
+# Dataset is on SharePoint site for the SMSCG action
+
+# Define path on SharePoint site for data
+sharepoint_path <- normalizePath(
+  file.path(
+    Sys.getenv("USERPROFILE"),
+    "California Department of Water Resources/SMSCG - Summer Action - Data"
+  )
+)  
+
+#read in phytoplankton community composition summary data
+#includes organisms/mL and biovolume/mL
+phyto_com<-read_csv(file = paste0(sharepoint_path,"./Phytoplankton/SMSCG_phytoplankton_sample_summary_2020.csv"))
+
+#read in extracted chlorophyll-a concentrations
+#NOTE: this code doesn't work 
+chlora<-read_excel(file = paste0(sharepoint_path,"./2020 data for USBR/DWR SuisunMarsh Chl All Stations WY2020.xlsx"))
