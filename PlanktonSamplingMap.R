c96bb154f9379a25c857887746b99c42d80fe205
Rasmussen <Nicholas.Rasmussen@water.ca.gov>
2022-03-14 15:55:00 -0700

    corrected GZM station location and formatted station labels on map

--- PlanktonSamplingMap.R
+++ PlanktonSamplingMap.R
@@ -12,7 +12,6 @@ library(ggrepel) #nonoverlapping point labels
 
 #Note: should add a start and end year to the stations file which will make it easier to work
 #with it in the future
-#update the GZM GPS coordinates: 38.13234, -122.05770 
 
 #read in data
 #get station names and coordinates from EDI
@@ -37,6 +36,11 @@ stations_p <- stations_all %>%
          StationCode != "D4" & StationCode!="NZ068" & StationCode!="707" & StationCode!="GRIZZ" & StationCode!="NZ060" & StationCode!="NZ064" & Longitude > -122.0833 &
            StationCode!="501" & StationCode!="504"
          ) %>% 
+  #correct location of GZM
+  mutate(
+    Longitude = ifelse(grepl("GZM",StationCode),-122.05770, Longitude)
+    ,Latitude = ifelse(grepl("GZM",StationCode),38.13234,Latitude)
+    ) %>% 
   #drop clam column
   select(-Clams) %>% 
   #convert coordinates data frame to sf object
@@ -73,7 +77,6 @@ stations$Type <- factor(stations$Type, levels=c('P','Z','B'))
 
 #plot bay-delta base layer with phytoplankton and zooplankton stations
 #move legend into plot space
-#work on station labels
 ggplot()+
   #plot waterways base layer
   geom_sf(data= WW_Delta, fill= "skyblue3", color= "black") +
@@ -86,26 +89,23 @@ ggplot()+
   scale_fill_manual(
     labels=c('Phyto','Zoop','Both'),
     values=c("#7FFF00","#CD6600","#7A378B"))+
-  #add station names as labels
-  #geom_sf_label(data = stations, aes(label = StationCode), #label the points
- #              nudge_x = -0.008, nudge_y = 0.008, size = 3, #adjust size and position relative to points
- #              inherit.aes = F, #tells it to look at points not base layer
- #              ) + 
-  geom_text_repel(data = stations, aes(x=Latitude,y=Longitude, label=StationCode), #label the points
-                nudge_x = -0.008, nudge_y = 0.008, size = 3, #adjust size and position relative to points
-                inherit.aes = F, #tells it to look at points not base layer
+  #add station names as labels and make sure they don't overlap each other or the points
+  geom_label_repel(data = stations, aes(x=Longitude,y=Latitude, label=StationCode) #label the points
+                #,nudge_x = -0.008, nudge_y = 0.008 #can specify the magnitude of nudges if necessary
+                , size = 3 #adjust size and position relative to points
+                ,inherit.aes = F #tells it to look at points not base layer
   ) + 
     #zoom in on region where stations are located using bounding box
     coord_sf( 
       xlim =c(bbox_p$xmin,bbox_p$xmax)
       ,ylim = c(bbox_p$ymin,bbox_p$ymax)
     )+
-  north(data = stations, symbol = 3) + #Add north arrow
+  north(data = stations, symbol = 12) + #Add north arrow
   theme(legend.position =c(-121.80, y = 38.20))+ #this isn't working
     theme_bw()+
   labs(x="Latitude",y="Longitude")+
   annotate("text", label = "2022 SMSCG Plankton Stations", x = -121.80, y = 38.20, size = 4)
-#ggsave(file = "SMSCG_Plankton_Map.png",type ="cairo-png",width=8.5, units="in",scale=1.5, dpi=300)
+#ggsave(file = "SMSCG_Plankton_Map.png",type ="cairo-png",width=8.5, units="in",scale=1, dpi=300)
 
 #make a map with just phyto samples 
 
