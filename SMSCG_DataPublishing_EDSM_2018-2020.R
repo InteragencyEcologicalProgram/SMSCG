1d43cb8fb091cb895e1eee0a20d67bf386635883
iep-coder <37346685+iep-coder@users.noreply.github.com>
2022-08-30 09:36:33 -0700

    script that pulls EDSM delta smelt data from EDI for publishing in SMSCG data package

--- SMSCG_DataPublishing_EDSM_2018-2020.R
+++ SMSCG_DataPublishing_EDSM_2018-2020.R
@@ -3,78 +3,70 @@
 #Prepare data associated with project for publishing 
 #in repository on Environmental Data Initiative
 
-#This file prepares data from the United States Fish and Wildlife Service
+#This script prepares data from the United States Fish and Wildlife Service
 #Enhanced Delta Smelt Monitoring (EDSM) surveys
 #https://www.fws.gov/lodi/juvenile_fish_monitoring_program/jfmp_index.htm
 
 #EDSM already has a repository on EDI with all their data
-#https://portal.edirepository.org/nis/mapbrowse?packageid=edi.415.3
+#https://portal.edirepository.org/nis/mapbrowse?scope=edi&identifier=415&revision=8
 
 #This code creates the subset relevant to the SMSCG action
-#Years: 2018, 2019, 2020 
-#Months: June - October
+#Years: 2018 and later
+#Months: July - October
 #Survey regions: Suisun Marsh, Suisun Bay, Lower Sacramento
 
 #required packages
 library(tidyverse)
 library(lubridate)
 
-#get most of EDSM kodiak trawl data from EDI
-#doesn't include most of 2020 data as of 2021-03-16
-#provisional version of 2020 data stored on SMSCG SharePoint site (received 2020-11-17)
-edsm_main<-read_csv("https://portal.edirepository.org/nis/dataviewer?packageid=edi.415.3&entityid=4d7de6f0a38eff744a009a92083d37ae") 
-#get a warning when importing data
-#Warning: 154432 parsing failures
+#get EDSM kodiak trawl data from EDI
+edsm_main<-read_csv("https://portal.edirepository.org/nis/dataviewer?packageid=edi.415.8&entityid=4d7de6f0a38eff744a009a92083d37ae") 
+#glimpse(edsm_main)
 
-#look at parsing failures from EDI data set--------------
+#filter to just the needed years, months, regions, and columns
+edsm_sub <- edsm_main %>% 
+  #create week, month, and year columns from date column
+  mutate(Year = year(SampleDate)
+         ,Month = month(SampleDate)
+         ,Week = week(SampleDate)
+         ) %>%
+  #filter to only keep desired years, months, and strata
+  filter(Year >= "2018" 
+         & Month %in% c(7,8,9,10)         
+         & !is.na(TowNumber)
+         & Stratum %in% 
+           c("Cache Slough LI","Lower Sacramento","Sac DW Ship Channel","Suisun Bay/Marsh","Lower San Joaquin","Suisun Bay","Suisun Marsh")
+         ) %>% 
+  #only keep necessary columns
+  select(Stratum,StationCode,SampleDate,Week,Month,Year,TowNumber,OrganismCode,Count)
 
-#The problems() function can be used to create a data frame of the parsing failures
-parse_failures<-problems(edsm_main)
+#summarize number of tows per stratum and week
+edsm_sum_tow <- edsm_sub %>% 
+  distinct(Stratum,StationCode,SampleDate,Week,Month,Year,TowNumber) %>% 
+  group_by(Stratum, Week, Month, Year) %>%
+  summarize(Tows = length(TowNumber)) %>% 
+  arrange(Year,Week,Stratum)
 
-#look at columns with failures
-unique(parse_failures$expected) 
-#"1/0/T/F/TRUE/FALSE"
+#summarize number of stations per stratum and week
+edsm_sum_station <- edsm_sub %>% 
+  distinct(Stratum,StationCode,Week,Month,Year) %>% 
+  group_by(Stratum, Week, Month, Year) %>%
+  summarize(Stations = length(StationCode)) %>% 
+  arrange(Year,Week,Stratum)
 
-unique(parse_failures$col)
-#"SpecialStudy"   "PairedStudy"    "Tide"           "SpecialStudyID" "StartDepth" 
-#In short, R couldn't accurately determine a column type from the first 1,000 rows 
-#for these four columns because they were NAs
-#Could easily specify different column types for these columns if necessary
+#summarize number of delta smelt per stratum and week
+edsm_sum_dsm <- edsm_sub %>% 
+  group_by(Stratum, Week, Month, Year) %>%
+  summarize(DSM = sum(Count[which(OrganismCode == "DSM")])) %>% 
+  arrange(Year,Week,Stratum)
 
-#look at structure of EDI data frame-----------
+#combine the summarized data for tows, station, and delta smelt counts
+#then calculate number of delta smelt per tow
+list_df = list(edsm_sum_tow,edsm_sum_station,edsm_sum_dsm)
+edsm_comb <- list_df %>% 
+  reduce(left_join, by=c("Stratum","Year","Week","Month")) %>% 
+  mutate(DSMcpue = DSM/Tows) %>% 
+  glimpse()
 
-glimpse(edsm_main)
-#most of the column types look correct
-#importantly this appears true for the 'Date' column
-
-#look at range of dates for the data set
-range(edsm_main$Date)
-#"2016-12-15" "2020-03-26"
-#doesn't yet include bulk of 2020 data
-
-#Combine 2020 data set with rest of data-------
-
-
-#Create subset of data set relevant to SMSCG action------
-
-#Create data just for summer-fall (June to October)
-edsm_sub<-edsm_main %>% 
-  filter(
-    month(Date) %in% c(6:10)
-         )
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+#export data for publishing on EDI
+#write_csv(edsm_comb,"EDSM_2018-2021_SummerFall_DSM.csv")
