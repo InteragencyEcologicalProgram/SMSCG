7e394662e783abf4f024514e8540f9fdb5e6b421
iep-coder <37346685+iep-coder@users.noreply.github.com>
2022-09-22 14:15:47 -0700

    Reformatted dates in zooplankton and gates log files

---
+++ EDI/data_output/README.txt
@@ -0,0 +1,16 @@
+Data files formatted using significant amounts of code:
+ - EDSM_2018-2021_SummerFall_DSM.csv
+ - SMSCG_phytoplankton_formatted_2020-2021.csv
+ - SMSCG_clam_EDI_2018_2021.csv
+
+Data files formatted using minimal amounts of code (eg, reformatting dates):
+ - SMSCG_GateDailyLog_1988-2021.csv
+ - SMSCG_Zooplankton_CBNet_2018to2021.csv
+ - SMSCG_Zooplankton_Mysid_2018to2019.csv
+
+Data files placed in this folder without additional formatting:
+ - FMWT_STN_ZoopTaxon_Lookup.csv
+ - stations_SMSCG.csv
+
+Data files missing from this folder (ie, file size too large for GitHub; on SharePoint instead):
+ - WQdata_smscg_2018-2021_FlaggedBadRemoved_clean.csv
---
+++ EDI/scripts/SMSCG_DataFormatting_Zoop&GateLog.R
@@ -0,0 +1,52 @@
+#Suisun Marsh Salinity Control Gate
+#EDI data files
+
+#Just need to format date for a few files
+#zoop and gate log
+
+#required packages
+library(tidyverse) #suite of data science tools
+library(lubridate) #working with dates
+
+#read in data
+zoop_cb <- read_csv("./EDI/data_input/zooplankton/SMSCG_CBNet_2018to2021CPUE_7April2022.csv") 
+
+#2018-2019 mysid data
+zoop_mysid <- read_csv("./EDI/data_input/zooplankton/SMSCG_MysidNet_CPUE_2018to2019_24Jan2022.csv") 
+#glimpse(zoop_mysid)
+
+#gates operations log
+gatelog <- read_csv("./EDI/data_input/gatedailylog_1988-2021.csv") 
+#glimpse(gatelog)
+
+#format dates-------------------
+
+#format CB zoop data date
+zoop_cb2 <- zoop_cb %>% 
+  mutate(Date = mdy(Date)) %>% 
+  glimpse()
+
+#format mysid data date
+zoop_mysid2 <- zoop_mysid %>% 
+  mutate(Date = mdy(Date)) %>% 
+  glimpse()
+
+#format gates operations data
+gatelog2 <- gatelog %>% 
+  #format date
+  mutate(Date = mdy(Date)) %>% 
+  #drop a row of NAs
+  filter(!is.na(Date)) %>% 
+  glimpse()
+
+#write updated files----------
+
+#write_csv(gatelog2,"./EDI/data_output/SMSCG_GateDailyLog_1988-2021.csv")
+
+#write_csv(zoop_cb2,"./EDI/data_output/SMSCG_Zooplankton_CBNet_2018to2021.csv")
+
+#write_csv(zoop_mysid2,"./EDI/data_output/SMSCG_Zooplankton_Mysid_2018to2019.csv")
+
+
+
+
