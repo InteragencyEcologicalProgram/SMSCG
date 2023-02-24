15d7a6e1307c84bca94df41acf0974aeae5d92f5
iep-coder <37346685+iep-coder@users.noreply.github.com>
2023-02-23 17:16:00 -0800

    started script to look at zoop sampling via power analysis

---
+++ PowerAnalysis/SMSCG_Zooplankton_PowerAnalysis.R
@@ -0,0 +1,14 @@
+#Suisun Marsh Salinity Control Gate
+#Zooplankton
+#Power analysis to determine amount of required sampling
+
+#required packages
+library(tidyverse) #suite of data science tools
+library(janitor) #functions for cleaning up data sets
+library(lubridate) #working with dates
+library(pwr) #power analysis
+
+#read in data--------------
+
+#results from the modeling that compared SMSCG action and non-action years
+
