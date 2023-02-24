2482dc25c0e4e6d37573171eb909ce8220f43f19
iep-coder <37346685+iep-coder@users.noreply.github.com>
2023-02-23 17:31:04 -0800

    started formatting zoop model estimates

--- PowerAnalysis/SMSCG_Zooplankton_PowerAnalysis.R
+++ PowerAnalysis/SMSCG_Zooplankton_PowerAnalysis.R
@@ -6,9 +6,38 @@
 library(tidyverse) #suite of data science tools
 library(janitor) #functions for cleaning up data sets
 library(lubridate) #working with dates
+library(zooper) #Bay-Delta zooplankton data
 library(pwr) #power analysis
 
 #read in data--------------
 
 #results from the modeling that compared SMSCG action and non-action years
+modout <- read_csv("./PowerAnalysis/zooplanktonmodel.csv") %>% 
+  clean_names() %>% 
+  rename("yes_action" = smsc_gaction) %>% 
+  glimpse()
+
+#format the zoop modeling data------------
+#results are divided by month and taxa
+#presumably the values are estimated biomass of carbon
+#combine taxa within month but for now keep months separate
+
+#summarize action and no action results by month
+modout_sum_mo <- modout %>% 
+  group_by(month) %>% 
+  summarise()
+
+
+
+
+
+
+#get data from zooper package-----------------------
+
+
+
+
+
+
+
 
