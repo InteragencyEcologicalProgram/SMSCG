ed015037e93b8a9086d0bd717ef430ec6b044b40
iep-coder <37346685+iep-coder@users.noreply.github.com>
2023-03-30 16:47:34 -0700

    updated study plan map by adding rest of EMP stations

--- Maps/PlanktonSamplingMap.R
+++ Maps/PlanktonSamplingMap.R
@@ -26,9 +26,9 @@ st_crs(WW_Delta)
 #CRS = NAD83, which is different than our sample data points
 #EPSG: 4269
 
-#format the data set
+#format the data set for DFW map-------------
 
-stations_p <- stations_all %>%
+stations_dfw <- stations_all %>%
   #filter stations to just those used in 2020 for plankton sampling
   #add Grizzly Bay station (602) and SMSCG
   filter(Zoops == "Y" | Phyto == "Y" | StationCode == "602" ) %>% 
@@ -50,7 +50,32 @@ stations_p <- stations_all %>%
   rename(Station = StationCode) %>% 
   glimpse()
 
-#create separate dataframe for SMSCG to plot on map
+#format the data set for SMSCG study plan map-------------
+
+stations_plan <- stations_all %>%
+  #filter stations to just those used in 2020 for plankton sampling
+  #add Grizzly Bay station (602) and SMSCG
+  filter(Zoops == "Y" | Phyto == "Y" | StationCode == "602" ) %>% 
+  #filter some other stations that aren't currently sampled by DFW
+  #but keep the EMP stations
+  filter(StationCode!="HONK" & StationCode!="520" &
+           StationCode!="NZ068" & StationCode!="707" & StationCode!="GRIZZ" & Longitude > -122.0833 &
+           StationCode!="501" & StationCode!="504"
+  ) %>% 
+  #correct location of GZM
+  #mutate(
+  #  Longitude = ifelse(grepl("GZM",StationCode),-122.05770, Longitude)
+  #  ,Latitude = ifelse(grepl("GZM",StationCode),38.13234,Latitude)
+  #  ) %>% 
+  #drop clam column
+  select(-Clams) %>% 
+  #specify CRS WGS84 (EPSG = 4326)
+  st_as_sf(coords = c(x='Longitude',y='Latitude'), remove=F,crs = 4326) %>% 
+  #rename column
+  rename(Station = StationCode) %>% 
+  glimpse()
+
+#create separate dataframe for SMSCG to plot on map-----------
 smscg <- stations_all %>% 
   filter(StationCode=="SM14") %>% 
   #change Station name for gates
@@ -59,9 +84,9 @@ smscg <- stations_all %>%
   st_as_sf(coords = c(x='Longitude',y='Latitude'), remove=F,crs = 4326) 
   
 
-#add column to use for color coding stations
+#DFW map: add column to use for color coding stations
 #zoop only, phyto only (none currently), both
-stations <- stations_p %>% 
+stations <- stations_dfw %>% 
   #add column that combines survey and station name
   unite("Station_label","Project","Station", sep= " ",remove=F) %>%
   mutate(
@@ -81,16 +106,47 @@ stations <- stations_p %>%
     ) %>% 
   glimpse()
 
+#Study plan map: add column to use for color coding stations
+#zoop only, phyto only (none currently), both
+stations_plan_cat <- stations_plan %>% 
+  #add column that combines survey and station name
+  #unite("Station_label","Project","Station", sep= " ",remove=F) %>%
+  mutate(
+    #change the N to Y for phyto in two 602 stations
+    Phyto = ifelse(grepl("602",Station),"Y",Phyto)
+    #couldn't quickly figure out how to change 602 and 519 in same line
+    #change the N to Y for phyto in two 519 stations
+    ,Phyto = ifelse(grepl("519",Station),"Y",Phyto)
+    #change the N to Y for phyto in NZ028
+    ,Phyto = ifelse(grepl("NZ028",Station),"Y",Phyto)
+    #change the N to Y for phyto in NZ054
+    ,Phyto = ifelse(grepl("NZ054",Station),"Y",Phyto)
+    ,Type = as.factor(
+      if_else(Zoops=="Y" & Phyto == "Y", "B" 
+              #,if_else(Zoops=="N" & Phyto == "Y", "P"
+              ,if_else(Zoops=="Y" & Phyto == "N","Z","N")
+              #)
+      ) )  
+    #modify name for MONT
+    #,Station_label = if_else(Station_label == "STN MONT","MONT",Station_label)
+  ) %>% 
+  glimpse()
 
 #create object from bounding box for the stations
 #add a buffer around points to improve map aesthetic
 bbox_p <- st_bbox(st_buffer(stations,2000))
 
-#reorder station type factor levels for plotting
+#DFW: reorder station type factor levels for plotting
 stations$Type <- factor(stations$Type
                         , levels=c(
                           #'P',
                           'Z','B'))
+
+#Plan: reorder station type factor levels for plotting
+stations_plan_cat$Type <- factor(stations_plan_cat$Type
+                        , levels=c(
+                          'Z','B'))
+
 #Prepare shapefile for adding regions to map--------------
 
 #look at coordinate reference system (CRS) of regions and basemap
@@ -147,7 +203,7 @@ region_focal <- subregions_4326 %>%
 #overall this matches up pretty well with where our focal stations are
 
 
-#plot for field survey instructions------------------
+#DFW map------------------
 ggplot()+
   #plot waterways base layer
   geom_sf(data= WW_Delta, fill= "skyblue3", color= "black") +
@@ -190,10 +246,10 @@ ggplot()+
     theme_bw()+
   labs(x="Longitude",y="Latitude")+
   annotate("text", label = "2023 SMSCG Plankton Stations", x = -121.80, y = 38.20, size = 4)
-#ggsave(file = "SMSCG_Plankton_Map_Field_2023.png",type ="cairo-png", scale=2.5, dpi=300)
+#ggsave(file = "./Maps/SMSCG_Plankton_Map_Field_2023.png",type ="cairo-png", scale=2.5, dpi=300)
 
 
-#plot for study plan-------------------
+#Study plan map-------------------
 #includes shaded regions
 ggplot()+
   #plot waterways base layer
@@ -203,27 +259,22 @@ ggplot()+
           , fill = c("gold","orchid1", "chartreuse")
           , alpha=0.2)+
   #plot station locations using different shapes and colors for different types of stations
-  geom_sf(data= stations, aes(fill= Type, shape= Type), color= "black",  size= 2.5)+
+  geom_sf(data= smscg, fill = "black", shape = 23, color= "black",  size= 4.5)+
+  geom_sf(data= stations_plan_cat, aes(fill=Type,color=Type, shape=Project), size= 2.5)+
+  scale_shape_manual(
+    labels=c('EMP','FMWT','STN'),
+    values=c(21:23)
+  )+
+   scale_color_manual(
+   labels=c('Zoop','Zoop + Phyto'),
+    values=c("#CD6600","#7A378B")
+   )+
+  scale_fill_manual(
+    labels=c('Zoop','Zoop + Phyto'),
+    values=c("#CD6600","#7A378B")
+    )+
   #add point for SMSCG 
   geom_sf(data= smscg, fill = "black", shape = 23, color= "black",  size= 4.5)+
-  scale_shape_manual(
-    labels=c(
-      #'Phyto',
-      'Zoop','Zoop + Phyto'),
-    values=c(21:22)
-  )+
-  scale_fill_manual(labels=c(
-      #'Phyto',
-      'Zoop','Zoop + Phyto'),
-    values=c(
-      #"#7FFF00",
-      "#CD6600","#7A378B"))+
-                 #add station names as labels and make sure they don't overlap each other or the points
-  geom_label_repel(data = stations, aes(x=Longitude,y=Latitude, label=Station_label) #label the points
-                   #,nudge_x = 0.02, nudge_y = 0.02 #can specify the magnitude of nudges if necessary
-                   , size = 3 #adjust size and position relative to points
-                   ,inherit.aes = F #tells it to look at points not base layer
-  ) + 
   #add label for SMSCG
   geom_label_repel(data = smscg, aes(x=Longitude,y=Latitude, label=Station) #label the points
                    ,nudge_x = 0.04, nudge_y = 0.008 #can specify the magnitude of nudges if necessary
@@ -242,7 +293,7 @@ ggplot()+
   annotate("text", x = c(-121.78,-121.75,-121.98), y=c(38.19,38.13,38.02), label = c("Suisun Marsh","River","Suisun Bay"), size=8)+
   theme_bw()+
   labs(x="Longitude",y="Latitude")
-#ggsave(file = "SMSCG_Plankton_Map_Plan_2023.png",type ="cairo-png", scale=2.5, dpi=300)
+#ggsave(file = "./Maps/SMSCG_Plankton_Map_Plan_2023.png",type ="cairo-png", scale=1, dpi=300)
 
 
 
