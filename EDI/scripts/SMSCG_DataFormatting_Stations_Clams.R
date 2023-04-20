c016ff5f49de99d4aa7efe6fd52a1ab221647f2a
iep-coder <37346685+iep-coder@users.noreply.github.com>
2023-04-20 15:48:33 -0700

    mapped station coordinates to make sure there weren't any errors

--- EDI/scripts/SMSCG_DataFormatting_Stations_Clams.R
+++ EDI/scripts/SMSCG_DataFormatting_Stations_Clams.R
@@ -114,6 +114,42 @@ clam_format <- clam_cleaner %>%
   left_join(clam_cross) %>% 
   glimpse()
 
-#write
+#plot the stations to check for errors---------
+
+#look at WW_Delta base map CRS
+st_crs(WW_Delta)
+#CRS = NAD83, which is different than our sample data points
+#EPSG: 4269
+
+#change CRS of of basemap to match station data sets EPSG = 4326
+ww_delta_4326 <- st_transform(WW_Delta, crs = 4326)
+
+#format the station data set
+station_map <- clam_format %>%
+  #specify CRS WGS84 (EPSG = 4326)
+  st_as_sf(coords = c(x='longitude',y='latitude'), remove=F,crs = 4326) %>% 
+  glimpse()
+
+#create object from bounding box for the stations
+#add a buffer around points to improve map aesthetic
+bbox_p <- st_bbox(st_buffer(station_map,2000))
+
+#make map
+(map_region_all<-ggplot()+
+    #Delta waterways
+    geom_sf(data = ww_delta_4326, fill= "lightblue", color= "black")+
+    #plot station locations 
+    geom_sf(data= station_map, fill="black",shape = 21, color= "black",  size= 3.5)+
+    #zoom in on region where stations are located using bounding box
+    coord_sf( 
+      xlim =c(bbox_p$xmin,bbox_p$xmax)
+      ,ylim = c(bbox_p$ymin,bbox_p$ymax)
+    )+
+    labs(x="Longitude",y="Latitude")+
+    theme_bw()
+)
+#looks good
+
+#write the output file
 #write_csv(clam_format,"./EDI/data_output/smscg_stations_clams.csv")
 
