f5074fa3b40c794d712c678eed076755f3b6f698
iep-coder <37346685+iep-coder@users.noreply.github.com>
2023-05-08 11:59:21 -0700

    preliminary checks and formatting of updated WQ data set

--- EDI/scripts/SMSCG_DataFormatting_WaterQuality_Continuous.R
+++ EDI/scripts/SMSCG_DataFormatting_WaterQuality_Continuous.R
@@ -1,6 +1,6 @@
 #Suisun Marsh Salinity Control Gate
 #Continuous water quality data
-#Final formatting for EDI
+#Final QAQC and formatting for EDI
 #Just need to find and replace the micro symbol with "u"
 #because micro is apparently not allowed
 
@@ -13,16 +13,37 @@ library(tidyverse) #suite of data science tools
 sharepoint_path <- normalizePath(
   file.path(
     Sys.getenv("USERPROFILE"),
-    "California Department of Water Resources/SMSCG - Summer Action - Data Package 2021"
+    "California Department of Water Resources/SMSCG - Summer Action - Data Package 2022"
   )
 ) 
 
-wq<-read_csv(file = paste0(sharepoint_path,"./WQdata_smscg_2018-2021 (Flagged Bad Removed).csv"))
+wq<-read_csv(file = paste0(sharepoint_path,"./SMSG Data 2018-2022 (Flagged Bad Removed).csv"))
 
-#find and replace micro with "u"
+#make sure all the stations are in the file
+unique(wq$cdec_code)
+#"CSE" "NSL" "MSL" "HUN" "GOD" "BDL" "VOL" "GZB" "GZM" "TRB" "MAL" "GZL" "HON" "RYC" "SSI"
+#looks good
+
+#check that all parameters are in file
+unique(wq$analyte_name)
+# "Water Temperature"    "Specific Conductance" "Dissolved Oxygen"     "Chlorophyll"          "Turbidity"            "pH"          
+#looks good
+
+#check that the full range of dates are in file
+range(wq$time)
+#"2018-01-01 UTC" "2023-01-01 UTC"
+#range looks good; might need to work on time zone though
+
+#look at unit names
+unique(wq$unit_name)
+#"\xb0C"        "\xb5S/cm"     "mg/L"         "\xb5g/L"      "NTU"          "pH Units"     "% saturation" "FNU"     
+#a few of these need work; degree and micro symbols didn't translate
+
+#find and replace micro with "u" and drop degrees from Celsius
 wq_final <- wq %>% 
-  mutate(unit_name2 = case_when(grepl("S/cm", unit_name) ~ "uS/cm"
-                                ,grepl("g/L", unit_name) ~ "ug/L"
+  mutate(unit_name2 = case_when(grepl("\xb5S/cm", unit_name) ~ "uS/cm"
+                                ,grepl("\xb0C", unit_name) ~ "C"
+                                ,grepl("\xb5g/L", unit_name) ~ "ug/L" 
                                 ,TRUE ~ unit_name))  %>% 
   select(-unit_name) %>% 
   rename(unit_name = unit_name2) %>% 
@@ -35,6 +56,7 @@ wq_final <- wq %>%
   glimpse()
 
 unique(wq_final$unit_name)
-#"uS/cm" "ug/L"  "NTU"   "FNU"  
+#"C"            "uS/cm"        "mg/L"         "ug/L"         "NTU"          "pH Units"     "% saturation" "FNU"
+#looks good now
   
-#write_csv(wq_final,file = paste0(sharepoint_path,"./WQdata_smscg_2018-2021_FlaggedBadRemoved_clean.csv"))
+#write_csv(wq_final,file = paste0(sharepoint_path,"./smscg_data_water_quality.csv"))
