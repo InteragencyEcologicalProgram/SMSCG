fb7ee0aab05ade4f19049937e5a4e7a45b8ae107
iep-coder <37346685+iep-coder@users.noreply.github.com>
2023-10-20 15:03:15 -0700

    deleted versions of biovolume analysis without log tranformation and also deleted all cell count based analysis. also checked correlation analysis assumptions

--- SMSCG_PrimaryProducerComparison_DEMP.R
+++ SMSCG_PrimaryProducerComparison_DEMP.R
@@ -4,10 +4,24 @@
 #Nick Rasmussen
 #2023-10-18
 
+#to do list-------
+#maybe try looking at pheophytin too
+
+#ask sarah perry for the fluorescence data
+
+#look at relationships by region and maybe water year
+
+#look at relationships at different parts of the turbidity range
+
+#could focus on samples with little to no blue-green algae biovolume
+
+#check assumptions for analyses more thoroughly
+
 #Packages------------------
 library(tidyverse)
 library(lubridate)
 library(janitor)
+library(ggpubr) #qqplots
 
 #read in data------------
 
@@ -126,24 +140,36 @@ wp_mis <-anti_join(phyto_sum,wq_format)
 #then combine the wq and phyto info to see if there are date mismatches or missing samples
 
 #all: look at relationship between phyto biovolume and chlor-a--------------------
+#NOTE: initially looked at relationships between pairs of raw data but log transformation of both is better
+#NOTE: looked at total cells vs chlor-a and relationship was much worse, as expected
 
-#plot relationship between chlor-a and phyto biovolume
-# (plot_cb_all <- ggplot(wp_all, aes(x = chla, y = total_biovolume))+
-#     geom_point()
-# )
+#check assumptions
+
+#normal distribution of each of the two datasets
+#in summary, neither are normal even after log transform
+
+#chlor-a
+hist(wp_all$chla) #strongly clustered at low end with a few high values 
+hist(log(wp_all$chla)) #looks much closer to normal but still long right tail
+shapiro.test(wp_all$chla) #W = 0.22154, p-value < 2.2e-16
+shapiro.test(log(wp_all$chla)) #W = W = 0.9182, p-value < 2.2e-16
+ggqqplot(log(wp_all$chla),ylab = "chlor-a") #not great even after log transformation
+
+#biovolume
+hist(wp_all$total_biovolume) #strongly clustered at low end with a few high values 
+hist(log(wp_all$total_biovolume)) #looks much more normal 
+shapiro.test(wp_all$total_biovolume) #W = 0.29101, p-value < 2.2e-16
+shapiro.test(log(wp_all$total_biovolume)) #W = 0.99472, p-value = 1.839e-07
+ggqqplot(log(wp_all$total_biovolume),ylab = "biovolume") #doesn't look too bad
 
 #plot relationship between chlor-a and phyto biovolume with log transformations
 (plot_cb_all_log <- ggplot(wp_all, aes(x = log(chla), y = log(total_biovolume)))+
     geom_point()
 )
-#probably the better way to look at data; can maybe make out a relationship
 
-#look at correlation 
-#cor.test(wp_all$chla,wp_all$total_biovolume)
-#cor = 0.59 which isn't bad but has to be driven by a few high points
-
-#look at correlation after log transformation
-cor.test(log(wp_all$chla),log(wp_all$total_biovolume))
+#look at pearson correlation after log transformation
+#probably not appropriate because didn't meet normality assumption
+#cor.test(log(wp_all$chla),log(wp_all$total_biovolume))
 #cor = 0.42 which is so-so
 
 #why is x axis so long (over 250)?
@@ -153,24 +179,15 @@ cor.test(log(wp_all$chla),log(wp_all$total_biovolume))
 #there are 5 cases
 #x axis can accommodate these high points but there's no biovolume to match them, which is why they aren't on plot
 
+#look at kendall rank correlation after log transformation
+cor.test(log(wp_all$chla),log(wp_all$total_biovolume),method="kendall")
+#tau =0.2287946
+#much worse than pearson
 
-#all: look at relationship between phyto cells and chlor-a--------------------
-
-
-#plot relationship between chlor-a and phyto cell counts
-# (plot_cc_all <- ggplot(wp_all, aes(x = chla, y = total_cells))+
-#     geom_point()
-# )
-
-#plot relationship between chlor-a and phyto cell counts using log transformed data
-(plot_cc_all_log <- ggplot(wp_all, aes(x = log(chla), y = log(total_cells)))+
-    geom_point()
-)
-#probably the better way to look at data; probably not a significant relationship
-
-#look at correlation
-cor.test(log(wp_all$chla),log(wp_all$total_cells))
-#cor = 0.16 which is pretty low
+#look at spearman correlation after log transformation
+cor.test(log(wp_all$chla),log(wp_all$total_biovolume),method="spearman")
+#rho =0.3345833
+#between pearson and kendall
 
 #make vector of target EMP stations------------
 stn_emp <- stn %>% 
@@ -218,6 +235,7 @@ phyto_recent <- phyto_sum %>%
 
 
 #smscg: combine WQ and phyto--------------
+#NOTE: tried looking at cell counts vs chlor-a and relationship was worse
 #should just match by station and date
 #both df have 298 rows which is good sign
 
@@ -248,35 +266,8 @@ wp_mis_recent <-anti_join(wq_recent,phyto_sum) %>%
 cor.test(log(wp_recent$chla),log(wp_recent$total_biovolume))
 #cor = 0.25 which is pretty low
 
-#smscg: look at relationship between chlor-a and phyto cells----------
-(plot_cc_recent <- ggplot(wp_recent, aes(x = log(chla), y = log(total_cells)))+
-    geom_point()
-)
-#one really low value for cells
 
-cor.test(log(wp_recent$chla),log(wp_recent$total_cells))
-#cor = 0.23 which is pretty low
 
-#look at outlier
-wp_recent_drop <- wp_recent %>% 
-  mutate(total_cells_log = log(total_cells)) %>% 
-  filter(total_cells_log > 5)
-#there is a sample where total cells = 1 which must be wrong
-#lets remove it and redo analysis
-
-(plot_cc_recent_drop <- ggplot(wp_recent_drop, aes(x = log(chla), y = log(total_cells)))+
-    geom_point()
-)
-#one really low value for cells
-
-cor.test(log(wp_recent_drop$chla),log(wp_recent_drop$total_cells))
-#cor = 0.22 which is pretty low
-
-#maybe try looking at pheophytin too
-
-#ask sarah perry for the fluorescence data
-
-#look at relationships by region and maybe water year
 
 
   
