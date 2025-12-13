8526b1b17284c189614bd0fe23f5f47f16a9eff8
iep-coder <37346685+iep-coder@users.noreply.github.com>
2025-12-12 16:04:08 -0800

    minor edits to standardize station names

--- EDI/scripts/SMSCG_DataFormatting_Zoop&GateLog.R
+++ EDI/scripts/SMSCG_DataFormatting_Zoop&GateLog.R
@@ -14,8 +14,11 @@ library(lubridate) #working with dates
 zoop_cb <- read_csv("./EDI/data_input/zooplankton/SMSCG_CBNet_2018to2023CPUE_07Feb2024.csv") 
 
 #2018-2019 mysid data
-#zoop_mysid <- read_csv("./EDI/data_input/zooplankton/SMSCG_MysidNet_CPUE_2018to2019_24Jan2022.csv") 
+zoop_mysid <- read_csv("./EDI/data_input/zooplankton/SMSCG_MysidNet_CPUE_2018to2019_24Jan2022.csv") 
 #glimpse(zoop_mysid)
+
+#read in zoop station metadata file to make sure the metadata and data files match up
+zoop_stn <- read_csv("./EDI/data_output/smscg_stations_zoop.csv")
 
 #gates operations log
 gatelog <- read_csv("./EDI/data_input/SMSCG_GateDailyLog_1988-2023.csv") 
@@ -31,9 +34,27 @@ zoop_cb2 <- zoop_cb %>%
   glimpse()
 
 #format mysid data date
-#zoop_mysid2 <- zoop_mysid %>% 
- # mutate(Date = mdy(Date)) %>% 
-  #glimpse()
+zoop_mysid2 <- zoop_mysid %>%
+mutate(Date = mdy(Date)) %>%
+glimpse()
+
+#look at unique mysid stations
+#unique(zoop_mysid2$Station)
+# "Honk" "Mont" "405"  "411"  "501"  "504"  "416"  "602"  "606"  "802"  "508"  "513"  "519"  "704" 
+#"706"  "707"  "520"  "609"  "610"  "MONT" "HONK"
+#need to format HONK and MONT so they are consistent
+
+#format mysid stations
+zoop_mysid3 <- zoop_mysid2 %>% 
+  mutate(Station = case_when(Station == "Mont" ~ "MONT"
+                             ,Station == "Honk" ~ "HONK"
+                             ,TRUE ~ Station))
+#unique(zoop_mysid3$Station)
+
+#try combining data and metadata and see if any mismatches
+#comb <- left_join(zoop_mysid3, zoop_stn)
+#station matched fine between dfs
+
 
 #format gates operations data
 gatelog2 <- gatelog %>% 
@@ -51,7 +72,7 @@ gatelog2 <- gatelog %>%
 
 #write_csv(zoop_cb2,"./EDI/data_output/SMSCG_Zooplankton_CBNet_2018-2023.csv")
 
-#write_csv(zoop_mysid2,"./EDI/data_output/SMSCG_Zooplankton_Mysid_2018to2019.csv")
+#write_csv(zoop_mysid3,"./EDI/data_output/SMSCG_Zooplankton_Mysid_2018to2019.csv")
 
 
 
