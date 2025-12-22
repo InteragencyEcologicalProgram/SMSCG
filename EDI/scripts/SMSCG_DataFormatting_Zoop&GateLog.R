e7015f6c35dbf4491512047f413f7b6f1f6fdbce
iep-coder <37346685+iep-coder@users.noreply.github.com>
2025-12-22 10:45:28 -0800

    small update to data file formatting script

--- EDI/scripts/SMSCG_DataFormatting_Zoop&GateLog.R
+++ EDI/scripts/SMSCG_DataFormatting_Zoop&GateLog.R
@@ -11,18 +11,18 @@ library(lubridate) #working with dates
 #read in data
 
 #all mesozooplankton data
-zoop_cb <- read_csv("./EDI/data_input/zooplankton/SMSCG_CBNet_2018to2023CPUE_07Feb2024.csv") 
+zoop_cb <- read_csv("./EDI/data_input/zooplankton/SMSCG_CBNet_2018to2024CPUE_14Apr2025.csv") 
 
 #2018-2019 mysid data
-zoop_mysid <- read_csv("./EDI/data_input/zooplankton/SMSCG_MysidNet_CPUE_2018to2019_24Jan2022.csv") 
+#zoop_mysid <- read_csv("./EDI/data_input/zooplankton/SMSCG_MysidNet_CPUE_2018to2019_24Jan2022.csv") 
 #glimpse(zoop_mysid)
 
 #read in zoop station metadata file to make sure the metadata and data files match up
-zoop_stn <- read_csv("./EDI/data_output/smscg_stations_zoop.csv")
+#zoop_stn <- read_csv("./EDI/data_output/smscg_stations_zoop.csv")
 
 #gates operations log
-gatelog <- read_csv("./EDI/data_input/SMSCG_GateDailyLog_1988-2023.csv") 
-glimpse(gatelog)
+gatelog <- read_csv("./EDI/data_input/SMSCG_GateDailyLog_1988-2024.csv") 
+#glimpse(gatelog)
 
 #format dates-------------------
 
@@ -34,9 +34,9 @@ zoop_cb2 <- zoop_cb %>%
   glimpse()
 
 #format mysid data date
-zoop_mysid2 <- zoop_mysid %>%
-mutate(Date = mdy(Date)) %>%
-glimpse()
+# zoop_mysid2 <- zoop_mysid %>%
+# mutate(Date = mdy(Date)) %>%
+# glimpse()
 
 #look at unique mysid stations
 #unique(zoop_mysid2$Station)
@@ -45,10 +45,10 @@ glimpse()
 #need to format HONK and MONT so they are consistent
 
 #format mysid stations
-zoop_mysid3 <- zoop_mysid2 %>% 
-  mutate(Station = case_when(Station == "Mont" ~ "MONT"
-                             ,Station == "Honk" ~ "HONK"
-                             ,TRUE ~ Station))
+# zoop_mysid3 <- zoop_mysid2 %>% 
+#   mutate(Station = case_when(Station == "Mont" ~ "MONT"
+#                              ,Station == "Honk" ~ "HONK"
+#                              ,TRUE ~ Station))
 #unique(zoop_mysid3$Station)
 
 #try combining data and metadata and see if any mismatches
@@ -63,14 +63,14 @@ gatelog2 <- gatelog %>%
          ,Date = as.character(Date)
          ) %>% 
   #drop a row of NAs
-  filter(!is.na(Date)) %>% 
+  #filter(!is.na(Date)) %>% 
   glimpse()
 
 #write updated files----------
 
-#write_csv(gatelog2,"./EDI/data_output/SMSCG_GateDailyLog_1988-2023.csv")
+#write_csv(gatelog2,"./EDI/data_output/SMSCG_GateDailyLog_1988-2024.csv")
 
-#write_csv(zoop_cb2,"./EDI/data_output/SMSCG_Zooplankton_CBNet_2018-2023.csv")
+#write_csv(zoop_cb2,"./EDI/data_output/SMSCG_Zooplankton_CBNet_2018-2024.csv")
 
 #write_csv(zoop_mysid3,"./EDI/data_output/SMSCG_Zooplankton_Mysid_2018to2019.csv")
 
